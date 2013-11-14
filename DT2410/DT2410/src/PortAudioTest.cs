16fdb25d2ace9e65c623cf481c4802fd3f829b4c
Sam Lewis <sam.vr.lewis@gmail.com>
2013-11-15 01:12:39 +1000

    some sort of double buffering semi-working

--- DT2410/DT2410/src/PortAudioTest.cs
+++ DT2410/DT2410/src/PortAudioTest.cs
@@ -2,18 +2,24 @@ using System;
 using System.Runtime.InteropServices;
 
 using PortAudioSharp;
+using System.IO;
+using NAudio.Wave;
 
 namespace PortAudioSharpTest
 {
 	
 	public class PortAudioTest
 	{
-	 	private float[] callbackBuffer = new float[512];
-	 	private int callbackPos = 0;
-	 	
+        private const int NUM_SAMPLES = 50000;
+
+        private float[] callbackBuffer = new float[NUM_SAMPLES];
+        short[][] sampleBuffer = new short[2][];
+        private int writeIndex = 1;
+
 	 	public PortAudioTest()
 		{ }
-	 
+
+
 	 	public PortAudio.PaStreamCallbackResult myPaStreamCallback(
 	 		IntPtr input,
 	 		IntPtr output,
@@ -23,20 +29,19 @@ namespace PortAudioSharpTest
 	 		IntPtr userData)
 	 	{
 	 		try {
-	 			// log("Callback called");
-		 		// log("time: " + timeInfo.currentTime 
-		 		// 		+ " " + timeInfo.inputBufferAdcTime
-		 		//		+ " " + timeInfo.outputBufferDacTime);
-		 		// log("statusFlags: "+statusFlags);
-		 		
+                
+                int readIndex = (writeIndex == 1) ? 0 : 1;
+                Console.WriteLine("Reading from " + readIndex.ToString());
 		 		if (callbackBuffer.Length < frameCount*2) 
 		 			callbackBuffer = new float[frameCount*2];
 		 	
 		 		for (int j = 0; j < frameCount*2; j++)
-					callbackBuffer[j] = (float) Math.Sin((double)(callbackPos++)/20.0);
-				
-				Marshal.Copy(callbackBuffer, 0, output, (int)frameCount*2);
-				
+					callbackBuffer[j] = Convert.ToSingle(sampleBuffer[readIndex][j++])/32767.0f;
+
+                writeIndex = readIndex;
+
+				Marshal.Copy(callbackBuffer, 0, output, (int)frameCount*2); 
+               
 	 		} catch (Exception e) { 
 	 			Console.WriteLine(e.ToString());
 	 		}
@@ -45,17 +50,51 @@ namespace PortAudioSharpTest
 	 	}
 		
 		public void Run() {
-			Audio audio = null;
+            //declare the sizes of the 2d array
+            sampleBuffer[0] = new short[NUM_SAMPLES];
+            sampleBuffer[1] = new short[NUM_SAMPLES];
+            
+            Audio audio = null;
+            WaveFileReader reader = new WaveFileReader("test.wav");
 
+            int readerPosition = 0;
+            int lastWritten = 0;
+
+            byte[] buffer = new byte[NUM_SAMPLES*2]; //buffer to read the wav raw bytes into
+            
+            //read out two buffers worth of samples
+            int read = reader.Read(buffer, 0, NUM_SAMPLES*2); 
+            Buffer.BlockCopy(buffer, 0, sampleBuffer[0], 0, read); 
+            readerPosition += read;
+           
 			try {
 			 
 				Audio.LoggingEnabled = true;
-				audio = new Audio(1, 2, 44100, 2048, new PortAudio.PaStreamCallbackDelegate(myPaStreamCallback));
-				
-				audio.Start();
-                
-                System.Threading.Thread.Sleep(3000);
+                audio = new Audio(1, 2, 44100, NUM_SAMPLES/2, new PortAudio.PaStreamCallbackDelegate(myPaStreamCallback));
+                audio.Start();
 
+                while (readerPosition < reader.Length)
+                {
+                   
+                    //wait for a buffer to be ready to be written into
+                    if (lastWritten != writeIndex)
+                    {
+                        Console.WriteLine("Writing to " + writeIndex.ToString());
+                        read = reader.Read(buffer, 0, NUM_SAMPLES*2); //read a block out
+
+                        Buffer.BlockCopy(buffer, 0, sampleBuffer[writeIndex], 0, read); //copy them to the short buffer
+
+                        readerPosition += read;
+                        lastWritten = writeIndex;
+                    }
+                }
+
+                while (lastWritten == writeIndex)
+                {
+                    System.Threading.Thread.Sleep(100);
+                    //wait for it to finish reading the final buffer
+                }
+                
 				audio.Stop();
 				
 			} catch(Exception e) {
