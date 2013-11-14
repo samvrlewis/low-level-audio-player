96ea85d448e6f0b188d5c892d471dc831ca28a47
Sam Lewis <sam.vr.lewis@gmail.com>
2013-11-15 04:25:53 +1000

    updated to use the pa stream rather than the audio class (more options)

--- DT2410/DT2410/src/PortAudioTest.cs
+++ DT2410/DT2410/src/PortAudioTest.cs
@@ -7,53 +7,69 @@ using NAudio.Wave;
 
 namespace PortAudioSharpTest
 {
-	
-	public class PortAudioTest
-	{
-        private const int NUM_SAMPLES = 10000;
 
-        private float[] callbackBuffer = new float[NUM_SAMPLES];
+    public class PortAudioTest
+    {
+        private const int NUM_SAMPLES = 25000;
+        private const short HALF_SHORT = 32767;
+
+        private short[] callbackBuffer = new short[NUM_SAMPLES];
         short[][] sampleBuffer = new short[2][];
         private int writeIndex = 1;
 
-	 	public PortAudioTest()
-		{ }
+        public PortAudioTest()
+        { }
 
 
-	 	public PortAudio.PaStreamCallbackResult myPaStreamCallback(
-	 		IntPtr input,
-	 		IntPtr output,
-	 		uint frameCount, 
-	 		ref PortAudio.PaStreamCallbackTimeInfo timeInfo,
-	 		PortAudio.PaStreamCallbackFlags statusFlags,
-	 		IntPtr userData)
-	 	{
-	 		try {
-                
+        public PortAudio.PaStreamCallbackResult myPaStreamCallback(
+            IntPtr input,
+            IntPtr output,
+            uint frameCount,
+            ref PortAudio.PaStreamCallbackTimeInfo timeInfo,
+            PortAudio.PaStreamCallbackFlags statusFlags,
+            IntPtr userData)
+        {
+            bool finished = false;
+
+            try
+            {
+
                 int readIndex = (writeIndex == 1) ? 0 : 1;
                 Console.WriteLine("Reading from " + readIndex.ToString());
-		 		if (callbackBuffer.Length < frameCount*2) 
-		 			callbackBuffer = new float[frameCount*2];
-		 	
-		 		for (int j = 0; j < frameCount*2; j++)
-					callbackBuffer[j] = Convert.ToSingle(sampleBuffer[readIndex][j++])/32767.0f;
+                
+
+                if (callbackBuffer.Length < frameCount * 2)
+                {
+                    callbackBuffer = new short[frameCount * 2];
+                    finished = true;
+                }
+
+                for (int j = 0; j < frameCount * 2; j++)
+                    callbackBuffer[j] = sampleBuffer[readIndex][j++];
+                
 
                 writeIndex = readIndex;
 
-				Marshal.Copy(callbackBuffer, 0, output, (int)frameCount*2); 
-               
-	 		} catch (Exception e) { 
-	 			Console.WriteLine(e.ToString());
-	 		}
-	 		 
-	 		return PortAudio.PaStreamCallbackResult.paContinue;
-	 	}
-		
-		public void Run() {
+                Marshal.Copy(callbackBuffer, 0, output, (int)frameCount * 2);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            //if (finished)
+              //  return PortAudio.PaStreamCallbackResult.paAbort;
+
+            return PortAudio.PaStreamCallbackResult.paContinue;
+        }
+
+        public void Run()
+        {
             //declare the sizes of the 2d array
             sampleBuffer[0] = new short[NUM_SAMPLES];
             sampleBuffer[1] = new short[NUM_SAMPLES];
-            
+
             Audio audio = null;
             string path = new DirectoryInfo(Environment.CurrentDirectory).Parent.Parent.FullName;
             WaveFileReader reader = new WaveFileReader(path + "/wave_files/test.wav");
@@ -61,27 +77,34 @@ namespace PortAudioSharpTest
             int readerPosition = 0;
             int lastWritten = 0;
 
-            byte[] buffer = new byte[NUM_SAMPLES*2]; //buffer to read the wav raw bytes into
-            
+            byte[] buffer = new byte[NUM_SAMPLES * 2]; //buffer to read the wav raw bytes into
+
             //read out two buffers worth of samples
-            int read = reader.Read(buffer, 0, NUM_SAMPLES*2); 
-            Buffer.BlockCopy(buffer, 0, sampleBuffer[0], 0, read); 
+            int read = reader.Read(buffer, 0, NUM_SAMPLES * 2);
+            Buffer.BlockCopy(buffer, 0, sampleBuffer[0], 0, read);
             readerPosition += read;
-           
-			try {
-			 
-				Audio.LoggingEnabled = true;
-                audio = new Audio(1, 2, 44100, NUM_SAMPLES/2, new PortAudio.PaStreamCallbackDelegate(myPaStreamCallback));
-                audio.Start();
+
+            try
+            {
+
+                PortAudio.Pa_Initialize();
+
+                IntPtr stream;
+                IntPtr userdata = IntPtr.Zero;
+
+                PortAudio.Pa_OpenDefaultStream(out stream, 1, 2, 8,
+                    48000, NUM_SAMPLES/2, new PortAudio.PaStreamCallbackDelegate(myPaStreamCallback), userdata);
+
+                PortAudio.Pa_StartStream(stream);
 
                 while (readerPosition < reader.Length)
                 {
-                   
+
                     //wait for a buffer to be ready to be written into
                     if (lastWritten != writeIndex)
                     {
                         Console.WriteLine("Writing to " + writeIndex.ToString());
-                        read = reader.Read(buffer, 0, NUM_SAMPLES*2); //read a block out
+                        read = reader.Read(buffer, 0, NUM_SAMPLES * 2); //read a block out
 
                         Buffer.BlockCopy(buffer, 0, sampleBuffer[writeIndex], 0, read); //copy them to the short buffer
 
@@ -90,22 +113,31 @@ namespace PortAudioSharpTest
                     }
                 }
 
-                while (lastWritten == writeIndex)
-                {
-                    
-                    //wait for it to finish reading the final buffer
-                }
+                //while (lastWritten == writeIndex)
+                //{
 
-                System.Threading.Thread.Sleep(5000);
+                    //wait for it to finish reading the final buffer
+                //}
+
+                //System.Threading.Thread.Sleep(5000);
+
+                while(PortAudio.Pa_IsStreamActive(stream) == 0)
+                {
+                    Console.WriteLine("waiting");
+
+                }
+                //PortAudio.Pa_StopStream(stream);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
                 
-				audio.Stop();
-				
-			} catch(Exception e) {
-				Console.WriteLine(e.ToString());
-			} finally {
-				if (audio != null) audio.Dispose();
-			}
-		}
-	}
-	
+            }
+        }
+    }
+
 }
