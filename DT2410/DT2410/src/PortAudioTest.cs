12c914dcb0d2e8d829225d63b2f94f75b821c18a
Sam Lewis <sam.vr.lewis@gmail.com>
2013-11-16 19:22:41 +1000

    used a queue rather than trying to implement double buffering myself. music playback mostly works.

--- DT2410/DT2410/src/PortAudioTest.cs
+++ DT2410/DT2410/src/PortAudioTest.cs
@@ -4,23 +4,25 @@ using System.Runtime.InteropServices;
 using PortAudioSharp;
 using System.IO;
 using NAudio.Wave;
+using System.Collections;
 
 namespace PortAudioSharpTest
 {
 
     public class PortAudioTest
     {
-        private const int NUM_SAMPLES = 25000;
-        private const short HALF_SHORT = 32767;
+        private const int NUM_SAMPLES = 2048;
+        private const int QUEUE_LENGTH = 20;
 
-        private short[] callbackBuffer = new short[NUM_SAMPLES];
-        short[][] sampleBuffer = new short[2][];
-        private int writeIndex = 1;
+        short[] sampleBuffer = new short[NUM_SAMPLES];
+        Queue sampleQueue = new Queue();
+
+        PortAudio.PaStreamCallbackDelegate callbackDelegate; //kept as a class variable so it doesn't get garbage collected
 
         public PortAudioTest()
         { }
 
-
+        
         public PortAudio.PaStreamCallbackResult myPaStreamCallback(
             IntPtr input,
             IntPtr output,
@@ -29,104 +31,72 @@ namespace PortAudioSharpTest
             PortAudio.PaStreamCallbackFlags statusFlags,
             IntPtr userData)
         {
-            bool finished = false;
+            Console.WriteLine("Reading");
+            short[] samplePacket;
 
-            try
+            if (sampleQueue.Count == 0)
             {
-
-                int readIndex = (writeIndex == 1) ? 0 : 1;
-                Console.WriteLine("Reading from " + readIndex.ToString());
-                
-
-                if (callbackBuffer.Length < frameCount * 2)
-                {
-                    callbackBuffer = new short[frameCount * 2];
-                    finished = true;
-                }
-
-                for (int j = 0; j < frameCount * 2; j++)
-                    callbackBuffer[j] = sampleBuffer[readIndex][j++];
-                
-
-                writeIndex = readIndex;
-
-                Marshal.Copy(callbackBuffer, 0, output, (int)frameCount * 2);
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
+                //this is likely a bad way of checking if the stream is complete as it could get in here if the read thread
+                //falls behind but it works behind. 
+                //todo: find a smarter way of knowing when the stream completes
+                return PortAudio.PaStreamCallbackResult.paComplete;
             }
 
-            //if (finished)
-              //  return PortAudio.PaStreamCallbackResult.paAbort;
+            samplePacket = (short[])sampleQueue.Dequeue();
+            Marshal.Copy(samplePacket, 0, output, (int)frameCount * 2);
 
             return PortAudio.PaStreamCallbackResult.paContinue;
         }
 
         public void Run()
         {
-            //declare the sizes of the 2d array
-            sampleBuffer[0] = new short[NUM_SAMPLES];
-            sampleBuffer[1] = new short[NUM_SAMPLES];
-
-            Audio audio = null;
-            string path = new DirectoryInfo(Environment.CurrentDirectory).Parent.Parent.FullName;
-            WaveFileReader reader = new WaveFileReader(path + "/wave_files/test.wav");
+            string rootPath = new DirectoryInfo(Environment.CurrentDirectory).Parent.Parent.FullName;
+            WaveFileReader reader = new WaveFileReader(rootPath + "/wave_files/jazzblues.wav");
+            callbackDelegate = new PortAudio.PaStreamCallbackDelegate(myPaStreamCallback); 
 
             int readerPosition = 0;
-            int lastWritten = 0;
-
             byte[] buffer = new byte[NUM_SAMPLES * 2]; //buffer to read the wav raw bytes into
-
-            //read out two buffers worth of samples
-            int read = reader.Read(buffer, 0, NUM_SAMPLES * 2);
-            Buffer.BlockCopy(buffer, 0, sampleBuffer[0], 0, read);
-            readerPosition += read;
+            int read = 0;
 
             try
             {
-
                 PortAudio.Pa_Initialize();
 
                 IntPtr stream;
-                IntPtr userdata = IntPtr.Zero;
+                IntPtr userdata = IntPtr.Zero; //intptr.zero is essentially just a null pointer
 
+                //for some reason .wavs seem to have half the sample rate that they should, i'm not sure quite why
+                //but we should find out
                 PortAudio.Pa_OpenDefaultStream(out stream, 1, 2, 8,
-                    48000, NUM_SAMPLES/2, new PortAudio.PaStreamCallbackDelegate(myPaStreamCallback), userdata);
+                    reader.WaveFormat.SampleRate/2, NUM_SAMPLES/2, callbackDelegate , userdata);
 
                 PortAudio.Pa_StartStream(stream);
 
                 while (readerPosition < reader.Length)
                 {
-
-                    //wait for a buffer to be ready to be written into
-                    if (lastWritten != writeIndex)
+                    //This is essentially working as a circular/FIFO buffer, where new sample packets are only added to the queue
+                    //if there's room in the queue. Once the packet is read out of the queue in the callback function it's 
+                    //removed from the queue and there is room to add more info to the queue
+                    if (sampleQueue.Count < QUEUE_LENGTH)
                     {
-                        Console.WriteLine("Writing to " + writeIndex.ToString());
-                        read = reader.Read(buffer, 0, NUM_SAMPLES * 2); //read a block out
-
-                        Buffer.BlockCopy(buffer, 0, sampleBuffer[writeIndex], 0, read); //copy them to the short buffer
-
+                        Console.WriteLine("Writing");
+                        short[] qBuffer = new short[NUM_SAMPLES]; //this has to be declared here as objects as sent by reference to the queue
+                        read = reader.Read(buffer, 0, NUM_SAMPLES * 2); //read a block of bytes out from the wav
+                        Buffer.BlockCopy(buffer, 0, qBuffer, 0, read); //copy them to a buffer of samples
+                        sampleQueue.Enqueue(qBuffer); //send the buffer to the queue
                         readerPosition += read;
-                        lastWritten = writeIndex;
                     }
+
                 }
 
-                //while (lastWritten == writeIndex)
-                //{
-
-                    //wait for it to finish reading the final buffer
-                //}
-
-                //System.Threading.Thread.Sleep(5000);
-
-                while(PortAudio.Pa_IsStreamActive(stream) == 0)
+                while(PortAudio.Pa_IsStreamActive(stream) != 0)
                 {
-                    Console.WriteLine("waiting");
-
+                    Console.WriteLine("waiting"); //this loop never seems to gets entered, think my comparison may be the problem
                 }
-                //PortAudio.Pa_StopStream(stream);
+                
+                PortAudio.Pa_StopStream(stream);
+
+                Console.ReadLine();
 
             }
             catch (Exception e)
