0732d206652a7c8f66cce9c83c047bd458d4fe18
Sam Lewis <sam.vr.lewis@gmail.com>
2013-12-10 05:18:01 +1000

    just fixing the name of a label

--- DT2410/DT2410/src/Form1.Designer.cs
+++ DT2410/DT2410/src/Form1.Designer.cs
@@ -112,11 +112,11 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(66, 186);
+            this.label2.Location = new System.Drawing.Point(90, 186);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(137, 13);
+            this.label2.Size = new System.Drawing.Size(89, 13);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Offset in number of samples";
+            this.label2.Text = "Offset in seconds";
             // 
             // btn_stop
             // 
